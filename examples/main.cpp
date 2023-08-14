662fef655cd49d354766ef8ea08b20ee323b7fa2
Kevin Channon <kevin.channon@paconsulting.com>
2023-08-14 08:56:29 +0100

    Add project files, test and example files

---
+++ examples/main.cpp
@@ -0,0 +1,7 @@
+// Copyright PA Knowledge 2023
+
+#include <mpsc/channel.hpp>
+
+auto main(int argc, char** argv) -> int {
+    return 0;
+}
---
+++ test/channel_tests.cpp
@@ -0,0 +1,9 @@
+// Copyright PA Knowledge 2023
+
+#include <mpsc/channel.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Channel tests") {
+    REQUIRE(true);
+}
