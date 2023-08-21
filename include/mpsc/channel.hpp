053ebcd91c9c415ff8fc7c8691e823b0917dd63f
Kevin Channon <kevin.channon@paconsulting.com>
2023-08-21 19:57:47 +0100

    Format doc-comment in channel.hpp with Doxygen syntax so that it's parsed properly by readers that can parse it.

--- include/mpsc/channel.hpp
+++ include/mpsc/channel.hpp
@@ -1,44 +1,54 @@
 ﻿/*
-mpsc_channel.hpp
-A simple C++ implementation of the 'channel' in rust language (std::mpsc).
+ * Copyright (c) 2019 liuchibing.
+ *
+ * This software is licensed under the MIT License.
+ * SPDX-License-Identifier: MIT
+ *
+ */
 
-# Usage
-Use `mpsc::make_channel<T>` to create a channel. `make_channel<T>` will return a tuple of (Sender<T>, Receiver<T>).
-
-For example:
-```c++
-// Create.
-auto [ sender, receiver ] = mpsc::make_channel<int>();
-
-// Send.
-sender.send(3);
-
-// Receive (both returns a std::optional<T>.)
-receiver.receive(); // Blocking when there is nothing present in the channel.
-receiver.try_receive(); // Not blocking. Return immediately.
-
-// close() and closed()
-sender.close();
-bool result = sender.closed();
-assert(result == receiver.closed());
-
-// You can use range-based for loop to receive from the channel.
-for (int v: receiver) {
-        // do something with v
-        // The loop will stop immedately after the sender called close().
-        // Only sender can call close().
-}
-```
-
-Note: `mpsc` stands for Multi-Producer Single-Consumer. So `Sender` can be either copied and moved, but `Receiver` can only be
-moved.
-
-Feel free to explore the `tests.cpp`. The tests are also examples of the usage.
-
-Read the source if you need more information. Sorry for the lack of comments. ～(￣▽￣～)~
-
-Copyright (c) 2019 liuchibing.
-*/
+/**
+ * @file channel.hpp
+ *
+ * @brief A simple C++ implementation of the 'channel' in Rust language (std::mpsc).
+ *
+ * # Usage
+ *
+ * Use `mpsc::make_channel<T>` to create a channel. `make_channel<T>` will return a tuple of (Sender<T>, Receiver<T>).
+ *
+ * For example:
+ *
+ * @code{.cpp}
+ * // Create.
+ * auto [sender, receiver] = mpsc::make_channel<int>();
+ *
+ * // Send.
+ * sender.send(3);
+ *
+ * // Receive (both returns a std::optional<T>.)
+ * receiver.receive(); // Blocking when there is nothing present in the channel.
+ * receiver.try_receive(); // Not blocking. Return immediately.
+ *
+ * // close() and closed()
+ * sender.close();
+ * bool result = sender.closed();
+ * assert(result == receiver.closed());
+ *
+ * // You can use range-based for loop to receive from the channel.
+ * for (int v: receiver) {
+ *   // do something with v
+ *
+ *   // The loop will stop immedately after the sender called close().
+ *   // Only sender can call close().
+ * }
+ * @endcode
+ *
+ * @note mpsc stands for Multi-Producer Single-Consumer. So Sender can be either
+ * copied and moved, but Receiver can only be moved.
+ *
+ * Feel free to explore the tests.cpp. The tests are also examples of the usage.
+ *
+ * Read the source if you need more information. Sorry for the lack of comments. ~( ̄▽ ̄)~
+ */
 
 #include <condition_variable>
 #include <exception>
